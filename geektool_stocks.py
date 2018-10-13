a411358ba606be6cc7898e4f87322eaaad99c8aa
abiekrinsky <abiekrinsky@gmail.com>
2018-10-12 18:02:16 -0700

    new updates

--- geektool_stocks.py
+++ geektool_stocks.py
@@ -13,15 +13,15 @@ Nas = noStarchSoup.select('span[data-reactid="18"]')
 if str(SnP[0])[66] == '-':
 	print('Not a good day for the S&P 500: ' + str(SnP[0])[66:72])
 elif str(SnP[0])[66] != '-':
-	print('S&P 500 is up!: ', + str(SnP[0])[66:71])
+	print('S&P 500 is up!: ' +  str(SnP[0])[68:74])
 
 if str(Dow[0])[67] == '-':
 	print('Not a good day for the Dow Jones: ' + str(Dow[0])[67:73])
 elif str(Dow[0])[67] != '-':
-	print('Dow Jones is up!: ' + str(Dow[0])[67:72])
+	print('Dow Jones is up!: ' + str(Dow[0])[69:75])
 
 if str(Nas[0])[67] == '-':
 	print('Not a good day for the Nasdaq: ' + str(Nas[0])[67:73])
 elif str(Dow[0])[67] != '-':
-	print('Nasdaq is up!: ' + str(Nas[0])[67:72])
+	print('Nasdaq is up!: ' + str(Nas[0])[69:75])
 
